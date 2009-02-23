9fccf0c1032b20bba35ae1a7a203e56a7faae527
Matthew Todd <matthew.todd@gmail.com>
2009-02-23 11:38:29 +0300

    Starting a unit test for our Swahili locale.

---
+++ test/unit/swahili_locale_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class SwahiliLocaleTest < ActiveSupport::TestCase
+  EN = YAML.load_file(Rails.root.join('config', 'locales', 'en.yml')).fetch('en')
+  SW = YAML.load_file(Rails.root.join('config', 'locales', 'sw.yml')).fetch('sw')
+
+  def self.should_have_the_same_structure_as(expected, actual, prefix=nil)
+    expected.each do |key, value|
+      should("have a translation for #{prefix}#{key}") { assert !actual[key].blank? }
+      should_have_the_same_structure_as(expected[key], actual[key], "#{prefix}#{key}.") if value.is_a?(Hash)
+    end
+  end
+
+  should_have_the_same_structure_as EN, SW
+end
